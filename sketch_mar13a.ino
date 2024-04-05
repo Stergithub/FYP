ef6314a27fbc89c24a590dbe36eb4278e1e94e36
Jagen Marvin <115367924+Jagen-marvin@users.noreply.github.com>
2024-04-05 12:27:49 +0300

    Update sketch_mar13a.ino

--- sketch_mar13a.ino
+++ sketch_mar13a.ino
@@ -8,12 +8,12 @@ const int TURBIDITY_POWER_PIN = D2;
 
 const int sensorPin = A0;
 
-// float calibration_value = 21.34 - 0.4;
-// int phval = 0; 
-// unsigned long int avgval; 
-// int buffer_arr[10],temp;
+float calibration_value = 21.34 - 0.4;
+int phval = 0; 
+unsigned long int avgval; 
+int buffer_arr[10],temp;
 
-// float ph_act;
+float ph_act;
 
 float volt;
 float ntu; 
@@ -32,38 +32,39 @@ void setup(){
 }
 
 void loop(){
-  // getPhSensorValue();
   // delay(500);
   getTurbiditySensorValue();
-  // delay(500);
+  delay(1000);
+  getPhSensorValue();
 }
 
-// void getPhSensorValue(){
-//   digitalWrite(PH_POWER_PIN, HIGH); //  Turn ph sensor On
-//   digitalWrite(TURBIDITY_POWER_PIN, LOW); // Turn turbidity senso Off
+void getPhSensorValue(){
+  digitalWrite(PH_POWER_PIN, HIGH); //  Turn ph sensor On
+  digitalWrite(TURBIDITY_POWER_PIN, LOW); // Turn turbidity senso Off
   
-//   for(int i=0;i<10;i++){ 
-//     buffer_arr[i]=analogRead(sensorPin);
-//     delay(30);
-//   }
-//   for(int i=0;i<9;i++){
-//     for(int j=i+1;j<10;j++){
-//       if(buffer_arr[i]>buffer_arr[j]){
-//         temp=buffer_arr[i];
-//         buffer_arr[i]=buffer_arr[j];
-//         buffer_arr[j]=temp;
-//       }
-//     }
-//   }
-//   avgval=0;
-//   for(int i=2;i<8;i++){
-//     avgval+=buffer_arr[i];
-//     float volt=(float)avgval*3.3/1023/6; 
-//     ph_act = -5.70 * volt + calibration_value;
-//   }
-//   Serial.print("pH Val: ");
-//   Serial.println(ph_act);
-// }
+  for(int i=0;i<10;i++){ 
+    buffer_arr[i]=analogRead(sensorPin);
+    delay(30);
+  }
+  for(int i=0;i<9;i++){
+    for(int j=i+1;j<10;j++){
+      if(buffer_arr[i]>buffer_arr[j]){
+        temp=buffer_arr[i];
+        buffer_arr[i]=buffer_arr[j];
+        buffer_arr[j]=temp;
+      }
+    }
+  }
+  avgval=0;
+  for(int i=2;i<8;i++){
+    avgval+=buffer_arr[i];
+    float volt=(float)avgval*3.0/1023/6; 
+    ph_act = -5.70 * volt + calibration_value;
+  }
+  Serial.print("pH Val: ");
+  Serial.println(ph_act);
+  delay(1000);
+}
 
 void getTurbiditySensorValue(){
   digitalWrite(TURBIDITY_POWER_PIN, HIGH); // Turn turbidity sensor On
